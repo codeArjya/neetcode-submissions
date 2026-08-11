9e24fe5110ada048fa3dbc659154c5ea79d9645e
Arjya Misra <71838473+codeArjya@users.noreply.github.com>
2026-08-11 18:42:37 -0400

    Add: two-integer-sum - submission-2

---
+++ Data Structures & Algorithms/two-integer-sum/submission-2.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i < nums.length; i++) {
+            int difference = target - nums[i];
+            if(map.containsKey(difference))
+                return new int[] {map.get(difference), i};
+            map.put(nums[i], i);
+        }
+        return new int[] {};
+    }
+}
+
+// first thought answer: sort array, have two integers initially pointing to start and end, decrementing end if sum is too large and incrementing start if sum is too low
